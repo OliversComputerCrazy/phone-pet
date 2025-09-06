f64e4f2eb81f1e9fb2c82a978f941a6e1a4ecc7e
OliversComputerCrazy <oliverjohnstone10@gmail.com>
2025-09-06 11:52:51 +0100

    added main scene

--- html/phone pet.service.worker.js
+++ html/phone pet.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1757154063|138837897';
+const CACHE_VERSION = '1757155702|1778616900';
 /** @type {string} */
 const CACHE_PREFIX = 'phone pet-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
